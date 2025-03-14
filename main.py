cc3657840c92e3c6d2e8e7b631e5c4d625d6eb20
tran-learner <lucian457fcde@gmail.com>
2025-03-14 15:42:14 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from routers.barista_web import barista_web_router
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Only accept request from this address
+    allow_credentials=True,
+    allow_methods=["*"],  # accept all methods
+    allow_headers=["*"],  # accept all headers
+)
+
+allow_origins=["http://localhost:3000"]
+@app.get("/")
+async def welcome():
+    return "Server is running at"
+app.include_router(barista_web_router)
