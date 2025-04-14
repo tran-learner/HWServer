5c34f79914d2e24a6cedf920663f7e3a1aec87e7
Your Name <you@example.com>
2025-04-14 12:29:35 +0700

    a

--- routers/barista_web.py
+++ routers/barista_web.py
@@ -1,5 +1,21 @@
+# from fastapi import APIRouter
+# from pydantic import BaseModel
+# barista_web_router = APIRouter()
+
+# class PumpData(BaseModel):
+#     sugar: int
+#     coffee: int
+
+# @barista_web_router.post('/pumphandle')
+# async def pump_handle(data: PumpData):
+#     print("Recieved:")
+#     print(f"Sugar: {data.sugar}")
+#     print(f"Coffee: {data.coffee}")
+
 from fastapi import APIRouter
 from pydantic import BaseModel
+from DemoStep import run_pumps
+
 barista_web_router = APIRouter()
 
 class PumpData(BaseModel):
@@ -8,6 +24,10 @@ class PumpData(BaseModel):
 
 @barista_web_router.post('/pumphandle')
 async def pump_handle(data: PumpData):
-    print("Recieved:")
+    print("Received:")
     print(f"Sugar: {data.sugar}")
     print(f"Coffee: {data.coffee}")
+    
+    run_pumps(data.coffee, data.sugar)
+
+    return {"status": "success"}
