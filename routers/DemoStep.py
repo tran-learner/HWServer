9fddaa7104514ba25fad4e50293a8e6e8306bb2c
Your Name <you@example.com>
2025-04-14 13:55:56 +0700

    a

--- routers/DemoStep.py
+++ routers/DemoStep.py
@@ -6,7 +6,9 @@ import threading
 step_enpin = [23, 25, 7, 12]
 step_dirpin = [24, 8, 1, 16]
 
-def run_step(pin, dir, steps, dutycycle=50):
+
+
+def run_step(pin, dir, steps, dutycycle=75):
     try:
         for step in range(steps):
             GPIO.output(step_dirpin[pin], dir)
@@ -23,7 +25,6 @@ def run_pumps(coffee: int, sugar: int):
         GPIO.setup(step_enpin[a], GPIO.OUT)
         GPIO.setup(step_dirpin[a], GPIO.OUT)
         GPIO.output(step_dirpin[a], 0)
-
     # Tạo và chạy các thread để điều khiển bơm
     thread1 = threading.Thread(target=run_step, args=(0, 1, coffee * 10))
     thread2 = threading.Thread(target=run_step, args=(1, 1, sugar * 10))
