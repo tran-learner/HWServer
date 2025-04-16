5428746afb26e147fbf1a65120462d75229b846b
Your Name <you@example.com>
2025-04-16 17:04:28 +0700

    a

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:8000",
+        "http://127.0.0.1:8000",
         "https://baristaweb.onrender.com/drinks"
     ],  # Only accept request from this address
     allow_credentials=True,
