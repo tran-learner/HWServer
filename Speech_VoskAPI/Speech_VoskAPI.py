2acfe1006ddda61f58204359f78a708670676d6e
NGUYENHIEN <trannguyenhien29085@gmail.com>
2025-04-25 12:46:33 +0700

    á

---
+++ Speech_VoskAPI/Speech_VoskAPI.py
@@ -0,0 +1,67 @@
+import sounddevice as sd
+import queue
+import json
+from vosk import Model, KaldiRecognizer
+
+# Confiuration Library
+model_path = "vosk-model-small-en-us-0.15"  
+# ======================================== Keywords Drinking Water ====================================
+keywords_milk_coffee = ["milk coffee","hot milk coffee","cappuccino","macchiato","coffee with milk"]
+keywords_coffee = ["coffee","cafe", "iced coffee", "espresso", "double espresso"]
+keywords_milk_tea = ["milk tea", "classic milk tea", "brown sugar milk tea","tea with milk"]
+# ======================================= Keywords Size =================================================
+keywords_Size_S = ["size S"]
+keywords_Size_M = ["size M"]
+keywords_Size_L = ["size L"]
+
+q = queue.Queue()
+
+device_info = sd.query_devices(sd.default.device[0], 'input')
+samplerate = int(device_info['default_samplerate'])
+
+model = Model(model_path)
+rec = KaldiRecognizer(model, samplerate)
+rec.SetWords(True)
+
+def callback(indata, frames, time, status):
+    if status:
+        print("Audio Error:", status)
+    q.put(bytes(indata))
+
+def contains_keywords(text, keywords):
+    for kw in keywords:
+        if kw in text:
+            return True
+    return False
+
+# ======================================== Run =========================================
+print(f"Listening... (sample rate = {samplerate})")
+with sd.RawInputStream(samplerate=samplerate, blocksize=8000, dtype='int16',
+                       channels=1, callback=callback):
+    while True:
+        data = q.get()
+        if rec.AcceptWaveform(data):
+            result = json.loads(rec.Result())
+            text = result.get("text", "").lower()
+            if text:
+                print("Speak:", text)
+
+                if contains_keywords(text, keywords_milk_coffee):
+                    print("Keyword Detect: milk coffee")
+
+                elif contains_keywords(text, keywords_coffee):
+                    print("Keyword Detect: coffee")
+
+                elif contains_keywords(text, keywords_milk_tea):
+                    print("Keyword Detect: milk tea")
+
+                elif contains_keywords(text, keywords_Size_S):
+                    print("Keyword Detect: milk tea")
+                    
+                elif contains_keywords(text, keywords_Size_M):
+                    print("Keyword Detect: milk tea")
+
+                elif contains_keywords(text, keywords_Size_L):
+                    print("Keyword Detect: milk tea")
+                else:
+                    print("Not Keyword Detect")
